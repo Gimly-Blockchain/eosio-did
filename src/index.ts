8dc9577123ddd374d44eb0a19ef3d94b2f3346f9
Jack Tanner <jack@gimly.io>
2021-05-06 17:29:55 +0200

    Added did-resolver and EosioDID class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,56 @@
+import { DIDDocument } from 'did-resolver';
 import { create } from './create';
 import { update } from './create';
 import { deactivate } from './create';
 
-export {
-  create,
-  resolve,
-  update,
-  deactivate
+type ConfigOptions = {
+  [x: string]: {
+    chainId: string,
+    service: [{
+      id: string,
+      type: [string],
+      serviceEndpoint: string
+    }]
+  }
+}
+
+type Authority = {
+  threshold: number,
+  keys?: [{
+    key: string,
+    weight: number
+  }],
+  accounts?: [{
+    permission: {
+      actor: string,
+      permission: string
+    },
+    weight: number
+  }],
+  waits?: [{
+    wait_sec: number,
+    wait: number
+  }]
+}
+
+export default class EosioDID {
+  constructor(options: ConfigOptions) {
+
+  }
+
+  async create(creator: string, name: string, owner: Authority, active: Authority, options?: ConfigOptions): Promise<DIDDocument> {
+    return { id: 'did:eosio:stub' };
+  }
+
+  async resolve(did: string, options?: ConfigOptions): Promise<DIDDocument> {
+    return { id: 'did:eosio:stub' };
+  }
+
+  async update(account: string, permission: string, parent: string, auth: Authority, options?: ConfigOptions): Promise<DIDDocument> {
+    return { id: 'did:eosio:stub' };
+  }
+
+  async deactivate(did: string, options?: ConfigOptions): Promise<void> {
+    throw Error('EOSIO DID deactivate not supported');
+  }
 }
